d6f11d982298691a84567e74ce0d880d2eb3e775
Alex Rael <alprael22@gmail.com>
2018-11-01 10:49:27 -0600

    implemented async task

--- app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/nasaapod/controller/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.cnm.deepdive.nasaapod.controller;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,13 +12,19 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.nasaapod.BuildConfig;
 import edu.cnm.deepdive.nasaapod.R;
 import edu.cnm.deepdive.nasaapod.model.Apod;
 import edu.cnm.deepdive.nasaapod.service.ApodService;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -92,5 +100,51 @@ public class MainActivity extends AppCompatActivity {
         .build();
     service = retrofit.create(ApodService.class);
     apiKey = BuildConfig.API_KEY;
+  }
+
+  private class ApodTask extends AsyncTask<Date, Void, Apod> {
+
+    private Date date;
+
+    @Override
+    protected void onPreExecute() {
+      progressSpinner.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void onPostExecute(Apod apod) {
+      MainActivity.this.apod = apod;
+      // TODO Handle hdUrl.
+      webView.loadUrl(apod.getUrl());
+    }
+
+    @Override
+    protected void onCancelled(Apod apod) {
+      Context context = MainActivity.this;
+      progressSpinner.setVisibility(View.GONE);
+      Toast.makeText(context, R.string.error_message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected Apod doInBackground(Date... dates) {
+      Apod apod = null;
+      try {
+        DateFormat format = new SimpleDateFormat(DATE_FORMAT);
+        date = (dates.length == 0) ? calendar.getTime() : dates[0];
+        Response<Apod> response = service.get(apiKey, format.format(date)).execute();
+        if (response.isSuccessful()) {
+          apod = response.body();
+          calendar.setTime(date);
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+        // Do nothing: apod is already null
+      } finally {
+        if (apod == null) {
+          cancel(true);
+        }
+      }
+      return apod;
+    }
   }
 }
